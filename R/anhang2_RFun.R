6766eb3c05d7703dd877d4528c91384724a9bebb
Matthias Gehrke <matthias.gehrke@fom.de>
2022-07-06 12:03:49 +0200

    R-Code aktualisiert (vgl. Buch) Datensätze ergänzt

--- R/anhang2_RFun.R
+++ R/anhang2_RFun.R
@@ -1,63 +1,4 @@
-# Optimaler Cutpoint gemaß Summe y = Summe yhat
-# notwendige Variablen: Modell, y-Variable, Wert für y = 1
-optCP <- function(mod, yvar, success){
-  # Variable mit 0 und 1 erzeugen
-  y <- ifelse(mod$model[yvar] == success, 1, 0)
-  sumy <- sum(y)
-  # cutpoint durchlaufen lassen
-  for(i in 0:10000){
-    cutpoint <- 0.0001 * i
-    ypred <- ifelse(fitted(mod) > cutpoint, 1, 0)
-    sumypred <- sum(ypred)
-    if(sumy >= sumypred) break
-  }
-  sprintf("Summe y: %d, Summe yhat: %d\n", sumy, sumypred) |> cat()
-  names(cutpoint) <- "Cutpoint"
-  return(cutpoint)
-}
-
-# Wu-Hausmann-Test 
-# H0: beide Modelle konsistent
-# Hinweis: funktioniert nicht bei bife- und glmer-Modellen
-whtest <- function(mod1, mod2){
-  # Koeffizienten
-  coef1 <- coef(mod1)
-  coef2 <- coef(mod2)
-  vcov1 <- vcov(mod1)
-  vcov2 <- vcov(mod2)
-  names1 <- names(coef1)
-  names2 <- names(coef2)
-  # nur gemeinsame Koeffizienten auswählen
-  # damit wird (Intercept) in REM sowie
-  # (Intercept) und sigma in logistischem REM gelöscht
-  if(length(names1) <= length(names2)){
-    # weniger oder gleich viele Koeffizienten in Modell 1
-    # Standard gemäß Formel
-    idx <- names2[names2 %in% names1]
-    # Differenz der Koeffizienten und der VCOV-Matrix bilden
-    dcoef <- coef2[idx] - coef1[idx]
-    dvcov <- vcov1[idx, idx] - vcov2[idx, idx]
-    # Hinweis zu HA ausgeben
-    cat("HA: Model 2 ist inkonsistent\n")
-  } else {
-    # weniger Koeffizienten in Modell 2
-    idx <- names1[names1 %in% names2]
-    # Differenz der Koeffizienten und der VCOV-Matrix bilden
-    dcoef <- coef1[idx] - coef2[idx]
-    dvcov <- vcov2[idx, idx] - vcov1[idx, idx]
-    # Hinweis zu HA ausgeben
-    cat("HA: Model 1 ist inkonsistent\n")
-  }
-  # Anzahl Freiheitsgrade
-  df <- length(dcoef)
-  # Teststatistik berechnen
-  chi2 <- t(dcoef) %*% solve(dvcov) %*% dcoef |> as.numeric()
-  # Überschreitungswahrscheinlichkeit Chi2-Verteilung
-  pval <- pchisq(chi2, df = df, lower.tail = FALSE)
-  # Ausgabe
-  sprintf("Chi2 = %.4f, df = %d, p-Wert = %.4g\n", chi2, df, pval) |> cat()
-}
-
+## R-Code B.1 ----
 # AIC, AICc und BIC in der Panelregression berechnen
 pAICBIC <- function(mod){
   # Residuen
@@ -105,6 +46,73 @@ pAICBIC <- function(mod){
   return(retval)
 }
 
+
+## R-Code B.2 ----
+# Optimaler Cutpoint gemaß Summe y = Summe yhat
+# notwendige Variablen: Modell, y-Variable, Wert für y = 1
+optCP <- function(mod, yvar, success){
+  # Variable mit 0 und 1 erzeugen
+  y <- ifelse(mod$model[yvar] == success, 1, 0)
+  sumy <- sum(y)
+  # cutpoint durchlaufen lassen
+  for(i in 0:10000){
+    cutpoint <- 0.0001 * i
+    # Typo korrigiert: KRED.glm6 durch mod ersetzt
+    ypred <- ifelse(fitted(mod) > cutpoint, 1, 0)
+    sumypred <- sum(ypred)
+    if(sumy >= sumypred) break
+  }
+  sprintf("Summe y: %d, Summe yhat: %d\n", sumy, sumypred) |> cat()
+  names(cutpoint) <- "Cutpoint"
+  return(cutpoint)
+}
+
+
+## R-Code B.3 ----
+# Wu-Hausmann-Test 
+# H0: beide Modelle konsistent
+# Hinweis: funktioniert nicht bei bife- und glmer-Modellen
+whtest <- function(mod1, mod2){
+  # Koeffizienten
+  coef1 <- coef(mod1)
+  coef2 <- coef(mod2)
+  vcov1 <- vcov(mod1)
+  vcov2 <- vcov(mod2)
+  names1 <- names(coef1)
+  names2 <- names(coef2)
+  # nur gemeinsame Koeffizienten auswählen
+  # damit wird (Intercept) in REM sowie
+  # (Intercept) und sigma in logistischem REM gelöscht
+  if(length(names1) <= length(names2)){
+    # weniger oder gleich viele Koeffizienten in Modell 1
+    # Standard gemäß Formel
+    idx <- names2[names2 %in% names1]
+    # Differenz der Koeffizienten und der VCOV-Matrix bilden
+    dcoef <- coef2[idx] - coef1[idx]
+    dvcov <- vcov1[idx, idx] - vcov2[idx, idx]
+    # Hinweis zu HA ausgeben
+    cat("HA: Model 2 ist inkonsistent\n")
+  } else {
+    # weniger Koeffizienten in Modell 2
+    idx <- names1[names1 %in% names2]
+    # Differenz der Koeffizienten und der VCOV-Matrix bilden
+    dcoef <- coef1[idx] - coef2[idx]
+    dvcov <- vcov2[idx, idx] - vcov1[idx, idx]
+    # Hinweis zu HA ausgeben
+    cat("HA: Model 1 ist inkonsistent\n")
+  }
+  # Anzahl Freiheitsgrade
+  df <- length(dcoef)
+  # Teststatistik berechnen
+  chi2 <- t(dcoef) %*% solve(dvcov) %*% dcoef |> as.numeric()
+  # Überschreitungswahrscheinlichkeit Chi2-Verteilung
+  pval <- pchisq(chi2, df = df, lower.tail = FALSE)
+  # Ausgabe
+  sprintf("Chi2 = %.4f, df = %d, p-Wert = %.4g\n", chi2, df, pval) |> cat()
+}
+
+
+## R-Code B.4 ----
 # AIC und ggf. BIC in der logistischen Panelregression berechnen
 lpAICBIC <- function(mod){
   # Devianz berechnen
@@ -130,26 +138,8 @@ lpAICBIC <- function(mod){
   return(retval)
 }
 
-# Optimale Anzahl von Lags für die Funktion Box.test
-optBT <- function(x, maxLags = 20, type = "Box-Pierce", fitdf = 0, flPrint = FALSE){
-  vals <- data.frame(lags = 0, pval = 1)
-  # Schleife von 1 bis maxLags
-  for(i in 1:maxLags){
-    # Box-Pierce- bzw. Ljung-Box-Test durchführen
-    out <- Box.test(DAX$Rendite, lag = i, type, fitdf)
-    # Ergebnis abspeichern
-    vals <- rbind(vals, data.frame(lags = out$parameter, pval = out$p.value))
-    # ggf. Ergebnis ausgeben
-    if(flPrint == TRUE){
-      sprintf("Lags: %d, p-Value: %.4f\n", out$parameter, out$p.value) |> cat()
-    }
-  }
-  # Lag für den kleinsten p-Wert ermitteln
-  minLags <- vals[which.min(vals$pval), "lags"]
-  # Box-Pierce- bzw. Ljung-Box-Test mit diesem Wert zurückgeben
-  return(Box.test(x, lag = minLags, type, fitdf))
-}
 
+## R-Code B.5 ----
 # abnormale Renditen berechnen
 getAR <- function(company, index, start, end){
   # Überprüfung, ob company und index xts-Objekte sind
@@ -166,6 +156,8 @@ getAR <- function(company, index, start, end){
   return(ar)
 }
 
+
+## R-Code B.6 ----
 # t-Test für abnormale Renditen
 tTestAR <- function(ar, wsstart, wsend, westart, weend, nParam = 2, flPlot = FALSE){
   # Schätzfenster
@@ -213,6 +205,8 @@ tTestAR <- function(ar, wsstart, wsend, westart, weend, nParam = 2, flPlot = FAL
   return(retval)
 }
 
+
+## R-Code B.7 ----
 # Corrado-Rang-Test für abnormale Renditen
 CRTestAR <- function(ar, wsstart, wsend, westart, weend, nParam = 2){
   # Schätzfenster
@@ -266,3 +260,26 @@ CRTestAR <- function(ar, wsstart, wsend, westart, weend, nParam = 2){
   
   return(retval)
 }
+
+
+## R-Code B.8 ----
+# Optimale Anzahl von Lags für die Funktion Box.test
+optBT <- function(x, maxLags = 20, type = "Box-Pierce", fitdf = 0, flPrint = FALSE){
+  vals <- data.frame(lags = 0, pval = 1)
+  # Schleife von 1 bis maxLags
+  for(i in 1:maxLags){
+    # Box-Pierce- bzw. Ljung-Box-Test durchführen
+    out <- Box.test(DAX$Rendite, lag = i, type, fitdf)
+    # Ergebnis abspeichern
+    vals <- rbind(vals, data.frame(lags = out$parameter, pval = out$p.value))
+    # ggf. Ergebnis ausgeben
+    if(flPrint == TRUE){
+      sprintf("Lags: %d, p-Value: %.4f\n", out$parameter, out$p.value) |> cat()
+    }
+  }
+  # Lag für den kleinsten p-Wert ermitteln
+  minLags <- vals[which.min(vals$pval), "lags"]
+  # Box-Pierce- bzw. Ljung-Box-Test mit diesem Wert zurückgeben
+  return(Box.test(x, lag = minLags, type, fitdf))
+}
+
